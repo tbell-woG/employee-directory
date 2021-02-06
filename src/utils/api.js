a92b8845002e6b3c96000f84d76e90e481e99fde
Tamira Bell <74482916+tbell-woG@users.noreply.github.com>
2021-02-05 22:05:07 -0500

    Decreased amount of employees listed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "https://randomuser.me/api/?results=50&nat=us"
+const URL = "https://randomuser.me/api/?results=30&nat=us"
 
 export default {
     getEmployees: function() {
